78573035518376d3f800b5c3649c56d54f542573
Cislove <vim152184@gmail.com>
2025-12-23 12:48:32 +0300

    init

---
+++ lib/fp_lab3.ex
@@ -0,0 +1,18 @@
+defmodule FpLab3 do
+  @moduledoc """
+  Documentation for `FpLab3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FpLab3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
