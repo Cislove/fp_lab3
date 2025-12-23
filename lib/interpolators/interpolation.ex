3c6c555f9984e08da98f173128c00e58111aee5f
Cislove <vim152184@gmail.com>
2025-12-23 22:03:49 +0300

    feat: add interpolation abstraction and lineal implementation

---
+++ lib/interpolators/interpolation.ex
@@ -0,0 +1,17 @@
+defmodule FpLab3.Interpolators.Interpolation do
+
+  @callback interpolate(points :: [tuple()], target_point :: number()) :: number()
+  @callback get_name() :: binary()
+  @callback get_points_enough() :: number()
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour FpLab3.Interpolators.Interpolation
+
+      @impl true
+      def interpolate(points, xs) when is_list(xs) do
+        Enum.map(xs, fn x -> interpolate(points, x) end)
+      end
+    end
+  end
+end
---
+++ lib/interpolators/lineal_interpolation.ex
@@ -0,0 +1,20 @@
+defmodule FpLab3.Interpolators.LinealInterpolation do
+
+  use FpLab3.Interpolators.Interpolation
+
+  @impl true
+  def get_name, do: "Линейная интерполяция"
+
+  @impl true
+  def get_points_enough(), do: 2
+
+  @impl true
+  def interpolate(points, x) when not is_list(x) do
+    [{x0, y0}, {x1, y1}] = points
+
+    a = (y1 - y0) / (x1 - x0)
+    b = y0 - a * x0
+
+    {x, a * x + b}
+  end
+end
